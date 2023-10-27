30fdf3985824f317c41a359dbd173f439c0d0afd
Hastin Modi <hastinmodi@gmail.com>
2023-10-27 16:34:30 -0700

    Modified data ingestion code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -21,8 +21,15 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('/Users/hastinmodi/Desktop/Projects/mlproject/notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
@@ -37,3 +44,8 @@ class DataIngestion:
             )
         except Exception as e:
             raise customException(e, sys)
+        
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
